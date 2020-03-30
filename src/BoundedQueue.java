f2a7361f4951fd2e75737ff95a87cfcbe09bfd9b
“CharlieYoung-007” <1172373115@qq.com>
2020-03-30 22:04:09 +0800

    I completed the partA,B,C

--- src/BoundedQueue.java
+++ src/BoundedQueue.java
@@ -4,56 +4,99 @@
  */
 public class BoundedQueue<T> {
 
+    private int capacity;
+    private boolean dropOldest;
+    private int countN = 0;
+    private T[] array;
+    static int BQNum = 0;
+    static int droppedNum = 0;
+
     /**
      * Constructs an empty BoundedQueue with the given capacity.
-     *
+     * <p>
      * The parameter dropOldest specifies what will happen when put()
      * is called and the queue is full. If dropOldest is true, the
      * oldest item (which would be returned by get()) is dropped. If
      * dropOldest is false, the new item is dropped.
      */
     public BoundedQueue(int capacity, boolean dropOldest) {
-        throw new RuntimeException("Not yet implemented");
+        this.capacity = capacity;
+        this.dropOldest = dropOldest;
+        makeArray(capacity);
+        BQNum++;
     }
 
     /**
      * Returns the number of items in the queue.
      */
     public int count() {
-        throw new RuntimeException("Not yet implemented");
+        for (int i = 0; i < capacity; i++) {
+            if (array[i] != null) {
+                countN++;
+            }
+        }
+        return countN;
     }
 
     /**
      * Returns true if the queue does not contain any items.
      */
     public boolean empty() {
-        throw new RuntimeException("Not yet implemented");
+        for (int i = 0; i < capacity; i++) {
+            if (array[i] != null) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
      * Returns true if the queue has reached its capacity.
      */
     public boolean full() {
-        throw new RuntimeException("Not yet implemented");
+        for (int i = 0; i < capacity; i++) {
+            if (array[i] == null) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
      * Adds the item to the queue.
-     *
+     * <p>
      * If the queue is full, an item is dropped as described in the
      * constructor.
      */
     public void put(T item) {
-        throw new RuntimeException("Not yet implemented");
+        for (int i = 0; i < capacity; i++) {
+            if (array[i] == null) {
+                array[i] = item;
+                break;
+            }
+            if (full()) {
+                get();
+            }
+        }
     }
 
     /**
      * Retrieves and removes the oldest item in the queue.
-     *
+     * <p>
      * If the queue is empty, returns null.
      */
     public T get() {
-        throw new RuntimeException("Not yet implemented");
+        if (array[0] == null) {
+            return null;
+        } else {
+            T num = array[0];
+            droppedNum++;
+            for (int i = 0; i < capacity - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            array[capacity - 1] = null;
+            return array[0];
+        }
     }
 
     /**
@@ -64,8 +107,12 @@ public class BoundedQueue<T> {
         // We would normally use "new T[size]", but we can't because T
         // is a type parameter and Java does not support generic array
         // creation. The following is an imperfect workaround.
-        @SuppressWarnings("unchecked")
-        T[] array = (T[]) new Object[size];
+        array = (T[]) new Object[size];
         return array;
+    }
+
+    static void reportStats(){
+        System.out.println("number of BoundedQueue instances: " + BQNum);
+        System.out.println("number of dropped items: " + droppedNum);
     }
 }
--- src/BoundedQueueTest.java
+++ src/BoundedQueueTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 public class BoundedQueueTest {
@@ -13,5 +15,56 @@ public class BoundedQueueTest {
         BoundedQueue<String> queue = new BoundedQueue<String>(3, false);
         queue.put("hello");
         assertTrue(!queue.empty(), "BoundedQueue should not be empty after an item is added");
+    }
+
+    @Test
+    public void newQueueIsNoItems() {
+        BoundedQueue<String> queue = new BoundedQueue<String>(3, false);
+        assertEquals(queue.count(),0,"BoundedQueue's items should be 0");
+    }
+
+    @Test
+    public void newQueueHasItems() {
+        BoundedQueue<String> queue = new BoundedQueue<String>(3, false);
+        queue.put("Charlie");
+        assertEquals(queue.count(),1,"BoundedQueue's items should not be 0 after an item is added");
+    }
+
+    @Test
+    public void newQueueNotFull() {
+        BoundedQueue<String> queue = new BoundedQueue<String>(3, false);
+        queue.put("Charlie");
+        assertFalse(queue.full(),"BoundedQueue return should not be true because the queue has not reached its capacity");
+    }
+
+    @Test
+    public void newQueueIsFull() {
+        BoundedQueue<String> queue = new BoundedQueue<String>(3, false);
+        queue.put("Charlie");
+        queue.put("Bob");
+        queue.put("Alice");
+        queue.put("Duke");
+        assertTrue(queue.full(),"BoundedQueue return should be true because the queue has reached its capacity");
+    }
+
+    @Test
+    public void newQueuePutOne() {
+        BoundedQueue<String> queue = new BoundedQueue<String>(3, false);
+        queue.put("Charlie");
+        assertEquals(queue.count(),1,"BoundedQueue items should 1 after 1 item is added");
+    }
+
+    @Test
+    public void newQueuePutTwo() {
+        BoundedQueue<String> queue = new BoundedQueue<String>(3, false);
+        queue.put("Charlie");
+        queue.put("Charlie");
+        assertEquals(queue.count(),2,"BoundedQueue items should 2 after 2 items are added");
+    }
+    
+
+    @AfterEach
+    public void reportStats() {
+        BoundedQueue.reportStats();
     }
 }
